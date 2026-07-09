a1028b541e25a470e70e3c376d3d38e668619dae
Finn191 <thanhphungab@gmail.com>
2026-07-09 14:49:03 +0700

    feat(board): add board runtime domain models

---
+++ Assets/FoodieMatch/Scripts/Core/Domain/Board/BoardModel.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+using FoodieMatch.Core.Domain.Grill;
+
+namespace FoodieMatch.Core.Domain.Board
+{
+    public sealed class BoardModel
+    {
+        private readonly List<GrillModel> _grills;
+
+        public BoardModel(IReadOnlyList<GrillModel> grills)
+        {
+            if (grills == null)
+            {
+                throw new ArgumentNullException(nameof(grills));
+            }
+
+            if (grills.Count == 0)
+            {
+                throw new ArgumentException(
+                    "Board must contain at least one grill.",
+                    nameof(grills));
+            }
+
+            _grills = CopyGrills(grills);
+        }
+
+        public int GrillCount => _grills.Count;
+
+        public bool HasRemainingFood
+        {
+            get
+            {
+                for (int i = 0; i < _grills.Count; i++)
+                {
+                    if (_grills[i].HasRemainingFood)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public int RemainingFoodCount
+        {
+            get
+            {
+                int count = 0;
+
+                for (int i = 0; i < _grills.Count; i++)
+                {
+                    count += _grills[i].RemainingFoodCount;
+                }
+
+                return count;
+            }
+        }
+
+        public bool CanRemoveFood(
+            FoodBoardAddress address,
+            int expectedFoodTokenId)
+        {
+            return address.IsValid &&
+                   TryGetGrill(
+                       address.GrillPositionIndex,
+                       out GrillModel grill) &&
+                   grill.CanRemoveFood(
+                       address.FoodSlotIndex,
+                       expectedFoodTokenId);
+        }
+
+        public bool TryRemoveFood(
+            FoodBoardAddress address,
+            int expectedFoodTokenId)
+        {
+            if (!address.IsValid ||
+                !TryGetGrill(
+                    address.GrillPositionIndex,
+                    out GrillModel grill))
+            {
+                return false;
+            }
+
+            return grill.TryRemoveFood(
+                address.FoodSlotIndex,
+                expectedFoodTokenId);
+        }
+
+        public bool TryGetGrill(
+            int positionIndex,
+            out GrillModel grill)
+        {
+            grill = null;
+
+            for (int i = 0; i < _grills.Count; i++)
+            {
+                if (_grills[i].PositionIndex == positionIndex)
+                {
+                    grill = _grills[i];
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IReadOnlyList<int> GetActiveFoodTokenIds()
+        {
+            List<int> foodTokenIds = new List<int>();
+
+            for (int grillIndex = 0; grillIndex < _grills.Count; grillIndex++)
+            {
+                GrillModel grill = _grills[grillIndex];
+
+                for (int slotIndex = 0;
+                     slotIndex < grill.ActiveFoodSlotCount;
+                     slotIndex++)
+                {
+                    int foodTokenId = grill.GetFoodTokenIdAt(slotIndex);
+
+                    if (foodTokenId > BoardRules.EmptyFoodTokenId)
+                    {
+                        foodTokenIds.Add(foodTokenId);
+                    }
+                }
+            }
+
+            return foodTokenIds;
+        }
+
+        public IReadOnlyList<int> GetTopTrayFoodTokenIds()
+        {
+            List<int> foodTokenIds = new List<int>();
+
+            for (int grillIndex = 0; grillIndex < _grills.Count; grillIndex++)
+            {
+                TrayModel topTray = _grills[grillIndex].TopTray;
+
+                if (topTray == null)
+                {
+                    continue;
+                }
+
+                for (int slotIndex = 0;
+                     slotIndex < topTray.SlotCount;
+                     slotIndex++)
+                {
+                    int foodTokenId = topTray.GetFoodTokenIdAt(slotIndex);
+
+                    if (foodTokenId > BoardRules.EmptyFoodTokenId)
+                    {
+                        foodTokenIds.Add(foodTokenId);
+                    }
+                }
+            }
+
+            return foodTokenIds;
+        }
+
+        private static List<GrillModel> CopyGrills(
+            IReadOnlyList<GrillModel> grills)
+        {
+            List<GrillModel> grillModels = new List<GrillModel>();
+            HashSet<int> positionIndexes = new HashSet<int>();
+
+            for (int i = 0; i < grills.Count; i++)
+            {
+                GrillModel grill = grills[i];
+
+                if (grill == null)
+                {
+                    throw new ArgumentException(
+                        "Board grill collection cannot contain null.",
+                        nameof(grills));
+                }
+
+                if (!positionIndexes.Add(grill.PositionIndex))
+                {
+                    throw new ArgumentException(
+                        "Board grill position indexes must be unique.",
+                        nameof(grills));
+                }
+
+                grillModels.Add(grill);
+            }
+
+            return grillModels;
+        }
+    }
+}
---
+++ Assets/FoodieMatch/Scripts/Core/Domain/Grill/GrillModel.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using FoodieMatch.Core.Domain.Board;
+
+namespace FoodieMatch.Core.Domain.Grill
+{
+    public sealed class GrillModel
+    {
+        private readonly int[] _activeFoodTokenIds;
+        private readonly List<TrayModel> _trays;
+
+        public GrillModel(
+            int positionIndex,
+            IReadOnlyList<int> activeFoodTokenIds,
+            IReadOnlyList<TrayModel> trays)
+        {
+            if (positionIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(positionIndex));
+            }
+
+            PositionIndex = positionIndex;
+            _activeFoodTokenIds = CopyFoodSlots(activeFoodTokenIds);
+            _trays = CopyTrays(trays);
+        }
+
+        public int PositionIndex { get; }
+        public int ActiveFoodSlotCount => _activeFoodTokenIds.Length;
+        public int TrayCount => _trays.Count;
+        public bool IsEmpty => ActiveFoodCount == 0;
+        public bool HasTrays => _trays.Count > 0;
+        public bool HasRemainingFood => !IsEmpty || HasTrays;
+        public TrayModel TopTray => HasTrays ? _trays[0] : null;
+
+        public int ActiveFoodCount
+        {
+            get
+            {
+                int count = 0;
+
+                for (int i = 0; i < _activeFoodTokenIds.Length; i++)
+                {
+                    if (_activeFoodTokenIds[i] > BoardRules.EmptyFoodTokenId)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public int RemainingFoodCount
+        {
+            get
+            {
+                int count = ActiveFoodCount;
+
+                for (int i = 0; i < _trays.Count; i++)
+                {
+                    count += _trays[i].FoodCount;
+                }
+
+                return count;
+            }
+        }
+
+        public int GetFoodTokenIdAt(int slotIndex)
+        {
+            return IsValidSlotIndex(slotIndex)
+                ? _activeFoodTokenIds[slotIndex]
+                : BoardRules.EmptyFoodTokenId;
+        }
+
+        public bool CanRemoveFood(
+            int slotIndex,
+            int expectedFoodTokenId)
+        {
+            return expectedFoodTokenId > BoardRules.EmptyFoodTokenId &&
+                   GetFoodTokenIdAt(slotIndex) == expectedFoodTokenId;
+        }
+
+        public bool TryRemoveFood(
+            int slotIndex,
+            int expectedFoodTokenId)
+        {
+            if (!CanRemoveFood(slotIndex, expectedFoodTokenId))
+            {
+                return false;
+            }
+
+            _activeFoodTokenIds[slotIndex] = BoardRules.EmptyFoodTokenId;
+            return true;
+        }
+
+        private bool IsValidSlotIndex(int slotIndex)
+        {
+            return slotIndex >= 0 && slotIndex < _activeFoodTokenIds.Length;
+        }
+
+        private static int[] CopyFoodSlots(
+            IReadOnlyList<int> foodTokenIds)
+        {
+            if (foodTokenIds == null)
+            {
+                throw new ArgumentNullException(nameof(foodTokenIds));
+            }
+
+            if (foodTokenIds.Count < BoardRules.MinFoodSlotCount ||
+                foodTokenIds.Count > BoardRules.MaxFoodSlotCount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(foodTokenIds));
+            }
+
+            int[] foodSlots = new int[foodTokenIds.Count];
+            bool hasFood = false;
+
+            for (int i = 0; i < foodTokenIds.Count; i++)
+            {
+                if (foodTokenIds[i] < BoardRules.EmptyFoodTokenId)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(foodTokenIds));
+                }
+
+                foodSlots[i] = foodTokenIds[i];
+                hasFood |= foodTokenIds[i] > BoardRules.EmptyFoodTokenId;
+            }
+
+            if (!hasFood)
+            {
+                throw new ArgumentException(
+                    "Grill must contain at least one active food token.",
+                    nameof(foodTokenIds));
+            }
+
+            return foodSlots;
+        }
+
+        private static List<TrayModel> CopyTrays(
+            IReadOnlyList<TrayModel> trays)
+        {
+            List<TrayModel> trayModels = new List<TrayModel>();
+
+            if (trays == null)
+            {
+                return trayModels;
+            }
+
+            for (int i = 0; i < trays.Count; i++)
+            {
+                if (trays[i] == null)
+                {
+                    throw new ArgumentException(
+                        "Tray collection cannot contain null.",
+                        nameof(trays));
+                }
+
+                trayModels.Add(trays[i]);
+            }
+
+            return trayModels;
+        }
+    }
+}
