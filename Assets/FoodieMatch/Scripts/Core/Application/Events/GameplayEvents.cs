bfe77b0f2d04fbd049b5f85c2fd0d631b9ac07be
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-07-16 14:34:00 +0700

    fix: :art: Go back to the main file to avoid conflicts

--- Assets/FoodieMatch/Scripts/Core/Application/Events/GameplayEvents.cs
+++ Assets/FoodieMatch/Scripts/Core/Application/Events/GameplayEvents.cs
@@ -1,35 +1,22 @@
 using System;
-
 namespace FoodieMatch.Core.Application.Events
 {
     public sealed class GameplayEvents
     {
         public event Action<LevelStartedEvent> LevelStarted;
-
         public event Action<LevelProgressChangedEvent> LevelProgressChanged;
-
         public event Action<LevelEndedEvent> LevelEnded;
-
-        public event Action<ComboChangedEvent> ComboChanged;
-
         public void OnLevelStarted(LevelStartedEvent eventData)
         {
             LevelStarted?.Invoke(eventData);
         }
-
         public void OnLevelProgressChanged(LevelProgressChangedEvent eventData)
         {
             LevelProgressChanged?.Invoke(eventData);
         }
-
         public void OnLevelEnded(LevelEndedEvent eventData)
         {
             LevelEnded?.Invoke(eventData);
-        }
-
-        public void OnComboChanged(ComboChangedEvent eventData)
-        {
-            ComboChanged?.Invoke(eventData);
         }
     }
 }
