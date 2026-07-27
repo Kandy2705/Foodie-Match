7de3a103c55c6ff14bda1a6213de7034c2fc496d
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-07-27 23:33:22 +0700

    feat(result): animate win reward buttons sequentially

--- Assets/FoodieMatch/Scripts/UI/Result/WinView.cs
+++ Assets/FoodieMatch/Scripts/UI/Result/WinView.cs
@@ -1,9 +1,9 @@
 using System;
 using FoodieMatch.UI.Popup;
+using PrimeTween;
 using Spine.Unity;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace FoodieMatch.UI.Result
@@ -12,6 +12,7 @@ namespace FoodieMatch.UI.Result
     {
         private const string WinAnimationName = "UI_win";
         private const string IdleAnimationName = "idle";
+        private const float ButtonOvershootScale = 1.3f;
 
         [Header("References")]
         [SerializeField] private Button _claimCoinRewardButton;
@@ -20,17 +21,32 @@ namespace FoodieMatch.UI.Result
         [SerializeField] private TMP_Text _doubleRewardAmountText;
         [SerializeField] private SkeletonGraphic _chefMascotSkeletonGraphic;
 
+        [Header("Button Reveal")]
+        [SerializeField, Min(0f)] private float _buttonRevealDelay = 0.2f;
+        [SerializeField, Min(0f)] private float _buttonScaleUpDuration = 0.18f;
+        [SerializeField, Min(0f)] private float _buttonSettleDuration = 0.12f;
+
         private Action _claimCoinRewardClicked;
         private Action _doubleCoinRewardClicked;
+        private Animator _claimCoinRewardButtonAnimator;
+        private Animator _doubleCoinRewardButtonAnimator;
+        private Sequence _buttonRevealSequence;
 
         private void Awake()
         {
+            _claimCoinRewardButtonAnimator =
+                _claimCoinRewardButton.GetComponent<Animator>();
+            _doubleCoinRewardButtonAnimator =
+                _doubleCoinRewardButton.GetComponent<Animator>();
+
             _claimCoinRewardButton.onClick.AddListener(OnClaimCoinRewardButtonClicked);
             _doubleCoinRewardButton.onClick.AddListener(OnDoubleCoinRewardButtonClicked);
+            PrepareRewardButtonsForReveal();
         }
 
         private void OnDestroy()
         {
+            StopButtonRevealAnimation();
             _claimCoinRewardButton.onClick.RemoveListener(OnClaimCoinRewardButtonClicked);
             _doubleCoinRewardButton.onClick.RemoveListener(OnDoubleCoinRewardButtonClicked);
         }
@@ -39,6 +55,14 @@ namespace FoodieMatch.UI.Result
         {
             base.Show();
             PlayWinMascotAnimation();
+            PlayButtonRevealAnimation();
+        }
+
+        public override void Hide()
+        {
+            StopButtonRevealAnimation();
+            RestoreRewardButtons();
+            base.Hide();
         }
 
         public void SetActions(WinViewActions actions)
@@ -86,6 +110,81 @@ namespace FoodieMatch.UI.Result
                 IdleAnimationName,
                 loop: true,
                 delay: 0f);
+        }
+
+        private void PlayButtonRevealAnimation()
+        {
+            StopButtonRevealAnimation();
+            PrepareRewardButtonsForReveal();
+
+            float revealDelay = Mathf.Max(0f, _buttonRevealDelay);
+            float scaleUpDuration = Mathf.Max(0f, _buttonScaleUpDuration);
+            float settleDuration = Mathf.Max(0f, _buttonSettleDuration);
+            Vector3 overshootScale = Vector3.one * ButtonOvershootScale;
+
+            _buttonRevealSequence = Sequence.Create(useUnscaledTime: true)
+                .ChainDelay(revealDelay)
+                .Chain(Tween.Scale(
+                    _doubleCoinRewardButton.transform,
+                    overshootScale,
+                    scaleUpDuration,
+                    Ease.OutQuad))
+                .Chain(Tween.Scale(
+                    _doubleCoinRewardButton.transform,
+                    Vector3.one,
+                    settleDuration,
+                    Ease.OutBack))
+                .Chain(Tween.Scale(
+                    _claimCoinRewardButton.transform,
+                    overshootScale,
+                    scaleUpDuration,
+                    Ease.OutQuad))
+                .Chain(Tween.Scale(
+                    _claimCoinRewardButton.transform,
+                    Vector3.one,
+                    settleDuration,
+                    Ease.OutBack))
+                .ChainCallback(this, view => view.EnableRewardButtons());
+        }
+
+        private void PrepareRewardButtonsForReveal()
+        {
+            SetRewardButtonAnimatorsEnabled(false);
+            _doubleCoinRewardButton.interactable = false;
+            _claimCoinRewardButton.interactable = false;
+            _doubleCoinRewardButton.transform.localScale = Vector3.zero;
+            _claimCoinRewardButton.transform.localScale = Vector3.zero;
+        }
+
+        private void EnableRewardButtons()
+        {
+            _doubleCoinRewardButton.interactable = true;
+            _claimCoinRewardButton.interactable = true;
+            SetRewardButtonAnimatorsEnabled(true);
+        }
+
+        private void RestoreRewardButtons()
+        {
+            SetRewardButtonAnimatorsEnabled(false);
+            _doubleCoinRewardButton.transform.localScale = Vector3.one;
+            _claimCoinRewardButton.transform.localScale = Vector3.one;
+            EnableRewardButtons();
+        }
+
+        private void SetRewardButtonAnimatorsEnabled(bool isEnabled)
+        {
+            _doubleCoinRewardButtonAnimator.enabled = isEnabled;
+            _claimCoinRewardButtonAnimator.enabled = isEnabled;
+        }
+
+        private void StopButtonRevealAnimation()
+        {
+            if (_buttonRevealSequence.isAlive)
+            {
+                _buttonRevealSequence.Stop();
+            }
+
+            _buttonRevealSequence = default;
         }
 
         private void OnClaimCoinRewardButtonClicked()
