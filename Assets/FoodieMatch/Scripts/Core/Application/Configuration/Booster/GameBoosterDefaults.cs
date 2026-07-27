10d3f0919ab078b6ae150a0fa5d9c97198513697
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-07-27 18:27:38 +0700

    chore(Feature): :art: Adjust the level corresponding to each booster

--- Assets/FoodieMatch/Scripts/Core/Application/Configuration/Booster/GameBoosterDefaults.cs
+++ Assets/FoodieMatch/Scripts/Core/Application/Configuration/Booster/GameBoosterDefaults.cs
@@ -5,10 +5,10 @@ namespace FoodieMatch.Core.Application.Configuration.Booster
 {
     public static class GameBoosterDefaults
     {
-        private const int DefaultPlateUnlockLevel = 1;
-        private const int DefaultStorageUnlockLevel = 1;
-        private const int DefaultSwapUnlockLevel = 5;
-        private const int DefaultFridgeUnlockLevel = 2;
+        private const int DefaultPlateUnlockLevel = 4;
+        private const int DefaultStorageUnlockLevel = 6;
+        private const int DefaultSwapUnlockLevel = 8;
+        private const int DefaultFridgeUnlockLevel = 10;
         private const int DefaultBoxUnlockLevel = 1;
 
         public static GameBoosterConfigSnapshot CreateSnapshot()
