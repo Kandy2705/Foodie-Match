780129abc0f3b31ea4263a9900ee89a0ee089977
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-08-03 17:37:03 +0700

    feat(UI): :art: Add RewardPreviewPanel when clicking the gift box

--- Assets/FoodieMatch/Scripts/UI/LeaderBoard/LeaderBoardPlayerRowView.cs
+++ Assets/FoodieMatch/Scripts/UI/LeaderBoard/LeaderBoardPlayerRowView.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,6 +11,39 @@ namespace FoodieMatch.UI.LeaderBoard
         [SerializeField] private TMP_Text _valueLabelText;
         [SerializeField] private TMP_Text _valueText;
         [SerializeField] private Image _avatarImage;
+        [SerializeField] private Button _giftButton;
+        [SerializeField] private Image _giftImage;
+
+        private Action<LeaderBoardPlayerRowView, RectTransform, int>
+            _giftClicked;
+        private int _rank;
+
+        private void OnEnable()
+        {
+            if (_giftButton != null)
+            {
+                _giftButton.onClick.AddListener(OnGiftButtonClicked);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_giftButton != null)
+            {
+                _giftButton.onClick.RemoveListener(OnGiftButtonClicked);
+            }
+
+        }
+
+        private void OnDestroy()
+        {
+            if (_giftButton != null)
+            {
+                _giftButton.onClick.RemoveListener(OnGiftButtonClicked);
+            }
+
+            _giftClicked = null;
+        }
 
         public virtual void Bind(
             LeaderBoardPlayerData player,
@@ -22,6 +56,46 @@ namespace FoodieMatch.UI.LeaderBoard
             _valueLabelText.text = valueLabel;
             _valueText.text = value.ToString();
             _avatarImage.sprite = avatar;
+            _rank = rank;
+        }
+
+        public void SetGiftClickHandler(
+            Action<LeaderBoardPlayerRowView, RectTransform, int>
+                giftClicked)
+        {
+            _giftClicked = giftClicked;
+        }
+
+        public void HideGift()
+        {
+            if (_giftImage == null)
+            {
+                return;
+            }
+
+            _giftImage.gameObject.SetActive(false);
+        }
+
+        protected void ShowGift(Sprite giftSprite)
+        {
+            _giftImage.sprite = giftSprite;
+            _giftImage.SetNativeSize();
+            _giftImage.rectTransform.localScale = Vector3.one;
+            _giftImage.gameObject.SetActive(true);
+        }
+
+        private void OnGiftButtonClicked()
+        {
+            if (_giftImage == null ||
+                !_giftImage.gameObject.activeInHierarchy)
+            {
+                return;
+            }
+
+            _giftClicked?.Invoke(
+                this,
+                _giftImage.rectTransform,
+                _rank);
         }
     }
 }
