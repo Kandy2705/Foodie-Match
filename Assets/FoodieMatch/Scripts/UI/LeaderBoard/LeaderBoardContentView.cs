d4fba6c2a09a7b2f039d9c7db0d8a3f47e98446a
Ngô Ngọc Triệu Mẫn <147357803+Kandy2705@users.noreply.github.com>
2026-08-12 15:51:21 +0700

    fix(UI): :art: add scroll reveal animation for top 3 leaderboard rows

--- Assets/FoodieMatch/Scripts/UI/LeaderBoard/LeaderBoardContentView.cs
+++ Assets/FoodieMatch/Scripts/UI/LeaderBoard/LeaderBoardContentView.cs
@@ -8,20 +8,45 @@ namespace FoodieMatch.UI.LeaderBoard
         [SerializeField] private ScrollRect _scrollRect;
         [SerializeField] private VerticalLayoutGroup _layoutGroup;
         [SerializeField] private ContentSizeFitter _contentSizeFitter;
-        [SerializeField] private LeaderBoardPlayerRowView[] _playerRows;
-        [SerializeField] private LeaderBoardPodiumPlayerView[]
-            _podiumPlayers;
-        [SerializeField] private RectTransform _podiumRoot;
-        [SerializeField] private CanvasGroup _podiumCanvasGroup;
+
+        [Header("Row Templates")]
+        [SerializeField]
+        private LeaderBoardMedalPlayerRowView _medalRowTemplate;
+
+        [SerializeField]
+        private LeaderBoardNumberedPlayerRowView _numberedRowTemplate;
+
+        [Header("Podium")]
+        [SerializeField]
+        private LeaderBoardPodiumPlayerView[] _podiumPlayers;
+
+        [SerializeField]
+        private RectTransform _podiumRoot;
+
+        [SerializeField]
+        private CanvasGroup _podiumCanvasGroup;
 
         public ScrollRect ScrollRect => _scrollRect;
-        public VerticalLayoutGroup LayoutGroup => _layoutGroup;
+
+        public VerticalLayoutGroup LayoutGroup =>
+            _layoutGroup;
+
         public ContentSizeFitter ContentSizeFitter =>
             _contentSizeFitter;
-        public LeaderBoardPlayerRowView[] PlayerRows => _playerRows;
+
+        public LeaderBoardMedalPlayerRowView MedalRowTemplate =>
+            _medalRowTemplate;
+
+        public LeaderBoardNumberedPlayerRowView NumberedRowTemplate =>
+            _numberedRowTemplate;
+
         public LeaderBoardPodiumPlayerView[] PodiumPlayers =>
             _podiumPlayers;
-        public RectTransform PodiumRoot => _podiumRoot;
-        public CanvasGroup PodiumCanvasGroup => _podiumCanvasGroup;
+
+        public RectTransform PodiumRoot =>
+            _podiumRoot;
+
+        public CanvasGroup PodiumCanvasGroup =>
+            _podiumCanvasGroup;
     }
 }
